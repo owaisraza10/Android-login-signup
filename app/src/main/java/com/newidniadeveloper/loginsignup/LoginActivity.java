782bbadfa310ba87acc7e8a641bafcf178a37cf0
Owais Raza <owaishraza53@gmail.com>
2022-11-07 14:13:35 +0530

    first commit

---
+++ app/src/main/java/com/newidniadeveloper/loginsignup/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.newidniadeveloper.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button nxtsgn_btn = findViewById(R.id.nxtsgn_btn);
+        nxtsgn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/newidniadeveloper/loginsignup/SignupActivity.java
@@ -0,0 +1,27 @@
+package com.newidniadeveloper.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Button nxtlgn_btn = findViewById(R.id.nxtlgn_btn);
+        nxtlgn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
